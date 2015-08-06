d4114516920e1fc46d10d05d3a55a2560b4d2a67
Sarah Guermond <s.guermond@gmail.com>
2015-08-06 15:35:18 -0700

    Add route for languages info pages

---
+++ lib/app/routes/languages.rb
@@ -0,0 +1,29 @@
+require 'app/presenters/problems'
+require 'app/presenters/tracks'
+
+module ExercismWeb
+	module Routes
+		class Languages < Core
+			get '/languages/:track_id' do |track_id|
+        
+        begin
+          active = ExercismWeb::Presenters::Tracks.find(track_id.to_s).active?
+          exists = true
+        rescue Exception => e
+          Bugsnag.notify(e)
+          "#{track_id} is not a language"
+          exists = false
+        end
+
+				locals = {
+          problems: Presenters::Problems.new(track_id).track_problems,
+		      language: Language.of(track_id),
+          slug: track_id,
+          active: active,
+          exists: exists
+			    }
+        erb :"languages/languages", locals: locals
+			end
+		end
+	end
+end
